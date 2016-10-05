c2f8c46da5c8481014761a302b5929c57b97fa38
Weston Ruter <weston@xwp.co>
2016-10-05 09:46:50 -0700

    Improve jetpack support; remove content_width setting

--- customize-adaptive-preview.php
+++ customize-adaptive-preview.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Customize Adaptive Preview
- * Description: Replace responsive device preview in customizer with adaptive preview, refreshing preview with query param indicating the device being previewed.
+ * Plugin Name: Customize Device Preview for RESS (Responsive Web Design + Server-Side Components)
+ * Description: Replace responsive device preview in customizer with RESS device preview, refreshing preview with query param indicating the device being previewed so that server-side components for the device can be previewed.
  * Version: 0.1.0
  * Author: XWP
  * Author URI: https://make.xwp.co/
@@ -27,40 +27,29 @@
 
 namespace Customize_Adaptive_Preview;
 
-/**
- * Check for dependencies.
- */
-function check_for_dependencies() {
-	if ( ! function_exists( 'jetpack_is_mobile' ) ) {
-		esc_html_e( 'Missing Jetpack dependency.', 'customize-adaptive-preview' );
-		trigger_error( 'missing_jetpack_dependency', E_USER_ERROR );
+const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36';
+const MOBILE_USER_AGENT = 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36';
+const TABLET_USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+
+// Override user agent if query param specifies as early as possible.
+if ( isset( $_GET['customize_previewed_device'] ) ) {
+	if ( 'desktop' === $_GET['customize_previewed_device'] ) {
+		$_SERVER['HTTP_USER_AGENT'] = wp_slash( DESKTOP_USER_AGENT );
+	} elseif ( 'tablet' === $_GET['customize_previewed_device'] ) {
+		$_SERVER['HTTP_USER_AGENT'] = wp_slash( TABLET_USER_AGENT );
+	} elseif ( 'mobile' === $_GET['customize_previewed_device'] ) {
+		$_SERVER['HTTP_USER_AGENT'] = wp_slash( MOBILE_USER_AGENT );
 	}
 }
-register_activation_hook( __FILE__, __NAMESPACE__ . '\check_for_dependencies' );
 
 /**
  * Init plugin.
  */
 function init() {
-	if ( ! function_exists( 'jetpack_is_mobile' ) ) {
-		add_action( 'admin_notices', __NAMESPACE__ . '\print_missing_dependency_admin_notice' );
-		return;
-	}
-	add_action( 'after_setup_theme', __NAMESPACE__ . '\set_content_width' );
 	add_action( 'customize_controls_enqueue_scripts', __NAMESPACE__ . '\customize_controls_enqueue_scripts' );
+	add_filter( 'jetpack_is_mobile', __NAMESPACE__ . '\filter_jetpack_is_mobile', 5, 3 );
 }
 add_action( 'plugins_loaded', __NAMESPACE__ . '\init' );
-
-/**
- * Print missing dependency admin notice.
- */
-function print_missing_dependency_admin_notice() {
-	?>
-	<div class="error">
-		<p><?php esc_html_e( 'Missing Jetpack dependency for Customize Adaptive Preview.', 'customize-adaptive-preview' ); ?></p>
-	</div>
-	<?php
-}
 
 /**
  * Enqueue scripts for customizer controls (pane).
@@ -78,6 +67,24 @@ function customize_controls_enqueue_scripts() {
 }
 
 /**
+ * Filter the value of jetpack_is_mobile before it is calculated.
+ *
+ * This is needed because the jetpack_is_mobile function sets
+ *
+ * @param bool|string $matches      Boolean if current UA matches $kind or not. If $return_matched_agent is true, should return the UA string.
+ * @param string      $kind         Category of mobile device being checked.
+ * @param bool        $return_agent Boolean indicating if the UA should be returned.
+ * @returns bool|string Whether matches or user agent.
+ */
+function filter_jetpack_is_mobile( $matches, $kind, $return_agent ) {
+	unset( $kind );
+	if ( isset( $_GET['customize_previewed_device'] ) && 'mobile' === $_GET['customize_previewed_device'] ) {
+		$matches = $return_agent ? MOBILE_USER_AGENT : true;
+	}
+	return $matches;
+}
+
+/**
  * Get current device.
  *
  * Obtain the device that is being used, whether by customizer preview or by device user agent.
@@ -85,31 +92,16 @@ function customize_controls_enqueue_scripts() {
  * @return string Device.
  */
 function get_current_device() {
-	$previewed_device = null;
-	if ( isset( $_GET['customize_previewed_device'] ) ) {
-		$previewed_device = sanitize_key( wp_unslash( $_GET['customize_previewed_device'] ) );
-	}
-	if ( empty( $previewed_device ) ) {
-		if ( jetpack_is_mobile( 'mobile' ) ) {
-			$previewed_device = 'mobile';
-		} elseif ( jetpack_is_mobile( 'tablet' ) ) {
+	$previewed_device = 'desktop';
+
+	if ( class_exists( 'Jetpack_User_Agent_Info' ) ) {
+		if ( \Jetpack_User_Agent_Info::is_tablet() ) {
 			$previewed_device = 'tablet';
-		} else {
-			$previewed_device = 'desktop';
+		} elseif ( jetpack_is_mobile() ) {
+			$previewed_device = 'mobile';
 		}
+	} elseif ( wp_is_mobile() ) {
+		$previewed_device = 'mobile';
 	}
 	return $previewed_device;
-}
-
-/**
- * Set content width.
- */
-function set_content_width() {
-	global $content_width;
-	$previewed_device = get_current_device();
-	if ( 'tablet' === $previewed_device ) {
-		$content_width = 768;
-	} elseif ( 'mobile' === $previewed_device ) {
-		$content_width = 320;
-	}
 }
